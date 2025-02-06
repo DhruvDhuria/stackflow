f9204daed2f662a6ca6998c744195b7d35166cd6
Dhruv Dhuria <dhuriadhruv13@gmail.com>
2025-02-06 14:37:02 +0530

    minor bug fix

--- src/app/questions/[quesId]/[quesName]/edit/page.tsx
+++ src/app/questions/[quesId]/[quesName]/edit/page.tsx
@@ -3,7 +3,7 @@ import { db, questionCollection } from "@/models/name";
 import EditQues from "./EditQues";
 import React from "react";
 
-const Page = async ({params}: {params: {quesId: string, quesName: string}}) => {
+const Page = async ({params}: {params: Promise<{quesId: string; quesName: string}>}) => {
     const {quesId} = await params
     const question = await databases.getDocument(db, questionCollection, quesId)
    
